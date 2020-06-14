5a72029f171718fffb62a205cfc1480e067826b6
BalramPrasad98 <50555118+BalramPrasad98@users.noreply.github.com>
2020-06-14 18:59:29 -0400

    Update settings.py

--- Gradestimator_Django/Gradestimator_Django/settings.py
+++ Gradestimator_Django/Gradestimator_Django/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Gradestimator_Django.urls'
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
